110df43d10e0e4b370458d72db3e59839c8910f0
ChoiGyuSong <cgs15002@gmail.com>
2023-11-13 15:20:05 +0900

    231113
    창고생성은됨
    (UI연결 안됨, 위치 지정 필요)

--- Assets/Script/Test/TestInventory.cs
+++ Assets/Script/Test/TestInventory.cs
@@ -7,29 +7,31 @@ using UnityEngine.UI;
 public class TestInventory : TestBase
 {
     public ItemManager inven;
+    public ObjectManager objManager;
 
     protected override void Test1(InputAction.CallbackContext _)
     {
-        inven.Add(Item.seaglider);
+        // inven.Add(Item.seaglider);
     }
 
     protected override void Test2(InputAction.CallbackContext _)
     {
-        inven.Add(Item.airtank);
+        // inven.Add(Item.airtank);
     }
 
     protected override void Test3(InputAction.CallbackContext _)
     {
-        inven.Add(Item.head);
+        // inven.Add(Item.head);
     }
 
     protected override void Test4(InputAction.CallbackContext _)
     {
-        inven.Add(Item.water);
+        // inven.Add(Item.water);
     }
 
     protected override void Test5(InputAction.CallbackContext _)
     {
+        objManager.AddGameObject();
     }
 
     protected override void TestRClick(InputAction.CallbackContext _)
