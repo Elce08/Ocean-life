dc77b9bee6827c7a6f3c1ab229d12f475414181a
Elce08 <yeonhahahahaha@gmail.com>
2023-12-05 14:01:40 +0900

    1205
    물고기 숫자 및 형태 변경 가능 수정

--- Assets/Script/Fish/Association.cs
+++ Assets/Script/Fish/Association.cs
@@ -8,75 +8,37 @@ public class Association : MonoBehaviour
 
     public float moveSpeed = 13.0f;
     public float sphereRadius = 5.0f;
+    GameObject[] fishs;
+    public int headNum = 3000;
+
+    public float xMultiple = 1.0f;
+    public float yMultiple = 1.0f;
+    public float zMultiple = 1.0f;
 
     private void Start()
     {
+        fishs = new GameObject[headNum];
         Spawn();
     }
 
     private void Spawn()
     {
-        GameObject[] fishs = new GameObject[3000];
-        for (int i = 0; i < 1000; i++)
+        SetSphere(0,(int)(headNum * 0.4),1.0f);
+        SetSphere((int)(headNum * 0.4),(int)(headNum * 0.7),0.75f);
+        SetSphere((int)(headNum * 0.7),(int)(headNum * 0.9),0.5f);
+        SetSphere((int)(headNum * 0.9),(headNum),0.25f);
+    }
+
+    private void SetSphere(int from, int to, float radiusMultiple)
+    {
+        for (int i = from; i < to; i++)
         {
             float theta = Random.Range(0f, Mathf.PI * 2);
-            float phi = Random.Range(0f, Mathf.PI);      
+            float phi = Random.Range(0f, Mathf.PI);
 
-            float x = sphereRadius * Mathf.Sin(phi) * Mathf.Cos(theta);
-            float y = sphereRadius * Mathf.Cos(phi);
-            float z = sphereRadius * Mathf.Sin(phi) * Mathf.Sin(theta);
-
-            Vector3 spawnPosition = new(x, y, z);
-            fishs[i] = Instantiate(fish, spawnPosition, Quaternion.identity, transform);
-            fishs[i].transform.parent = transform;
-        }
-        for (int i = 1000; i < 1800; i++)
-        {
-            float theta = Random.Range(0f, Mathf.PI * 2);
-            float phi = Random.Range(0f, Mathf.PI);      
-
-            float x = sphereRadius * 0.8f * Mathf.Sin(phi) * Mathf.Cos(theta);
-            float y = sphereRadius * 0.8f * Mathf.Cos(phi);
-            float z = sphereRadius * 0.8f * Mathf.Sin(phi) * Mathf.Sin(theta);
-
-            Vector3 spawnPosition = new(x, y, z);
-            fishs[i] = Instantiate(fish, spawnPosition, Quaternion.identity, transform);
-            fishs[i].transform.parent = transform;
-        }
-        for (int i = 1800; i < 2400; i++)
-        {
-            float theta = Random.Range(0f, Mathf.PI * 2);
-            float phi = Random.Range(0f, Mathf.PI);      
-
-            float x = sphereRadius * 0.6f * Mathf.Sin(phi) * Mathf.Cos(theta);
-            float y = sphereRadius * 0.6f * Mathf.Cos(phi);
-            float z = sphereRadius * 0.6f * Mathf.Sin(phi) * Mathf.Sin(theta);
-
-            Vector3 spawnPosition = new(x, y, z);
-            fishs[i] = Instantiate(fish, spawnPosition, Quaternion.identity, transform);
-            fishs[i].transform.parent = transform;
-        }
-        for (int i = 2400; i < 2800; i++)
-        {
-            float theta = Random.Range(0f, Mathf.PI * 2);
-            float phi = Random.Range(0f, Mathf.PI);      
-
-            float x = sphereRadius * 0.4f * Mathf.Sin(phi) * Mathf.Cos(theta);
-            float y = sphereRadius * 0.4f * Mathf.Cos(phi);
-            float z = sphereRadius * 0.4f * Mathf.Sin(phi) * Mathf.Sin(theta);
-
-            Vector3 spawnPosition = new(x, y, z);
-            fishs[i] = Instantiate(fish, spawnPosition, Quaternion.identity, transform);
-            fishs[i].transform.parent = transform;
-        }
-        for (int i = 2800; i <3000; i++)
-        {
-            float theta = Random.Range(0f, Mathf.PI * 2);
-            float phi = Random.Range(0f, Mathf.PI);      
-
-            float x = sphereRadius * 0.2f * Mathf.Sin(phi) * Mathf.Cos(theta);
-            float y = sphereRadius * 0.2f * Mathf.Cos(phi);
-            float z = sphereRadius * 0.2f * Mathf.Sin(phi) * Mathf.Sin(theta);
+            float x = sphereRadius * radiusMultiple * xMultiple * Mathf.Sin(phi) * Mathf.Cos(theta);
+            float y = sphereRadius * radiusMultiple * yMultiple * Mathf.Cos(phi);
+            float z = sphereRadius * radiusMultiple * zMultiple * Mathf.Sin(phi) * Mathf.Sin(theta);
 
             Vector3 spawnPosition = new(x, y, z);
             fishs[i] = Instantiate(fish, spawnPosition, Quaternion.identity, transform);
